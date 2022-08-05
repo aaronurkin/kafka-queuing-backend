22803bc7986fea19eebe350c8d4498ddc32d2065
Aaron Urkin <a.urkin@gmail.com>
2022-08-05 17:48:27 +0300

    Configure dummy Rails application for tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
 # frozen_string_literal: true
 
+require 'dotenv/load'
+
+# Configure Rails Environment
+ENV['RAILS_ENV'] = 'test'
+require_relative '../spec/dummy/config/environment'
+ENV['RAILS_ROOT'] ||= "#{File.dirname(__FILE__)}../../../spec/dummy"
+
 require 'kafka-queuing-backend'
